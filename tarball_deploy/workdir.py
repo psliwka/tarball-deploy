748d4236f02de693697d490e2526bf19ff0d5b7b
Piotr Śliwka <contact@psliwka.info>
2019-02-02 20:20:40 +0100

    Refactor

--- tarball_deploy/workdir.py
+++ tarball_deploy/workdir.py
@@ -12,17 +12,14 @@ class Workdir:
     def expand_subpath(self, path):
         return os.path.join(self.path, path)
 
-    def ensure_subpath(self, path):
-        os.makedirs(self.expand_subpath(path), exist_ok=True)
-
-    def new_release_dir(self):
+    def make_release_dir(self):
         name = uuid.uuid4()
-        subpath = f"releases/{name}"
-        self.ensure_subpath(subpath)
-        return subpath
+        release_dir = f"releases/{name}"
+        os.makedirs(self.expand_subpath(release_dir), exist_ok=True)
+        return release_dir
 
-    def unpack_new_release(self, data):
-        release_dir = self.new_release_dir()
+    def unpack_release(self, data):
+        release_dir = self.make_release_dir()
         subprocess.run(
             ["tar", "x", "-C", self.expand_subpath(release_dir), "-f", "-"],
             stdin=data,
@@ -30,23 +27,23 @@ class Workdir:
         )
         return release_dir
 
-    def update_link(self, name, target):
+    def make_link(self, name, target):
         with contextlib.suppress(FileNotFoundError):
             os.unlink(self.expand_subpath(name))
         os.symlink(target, self.expand_subpath(name))
 
-    def prepare_deployment(self, data):
-        release_dir = self.unpack_new_release(data)
-        self.update_link("next", release_dir)
+    def prepare_next_deployment(self, data):
+        release_dir = self.unpack_release(data)
+        self.make_link("next", release_dir)
 
-    def switch_to_deployment(self):
+    def switch_to_next_deployment(self):
         if os.path.exists(self.expand_subpath("current")):
-            self.update_link("previous", os.readlink(self.expand_subpath("current")))
+            self.make_link("previous", os.readlink(self.expand_subpath("current")))
         os.replace(self.expand_subpath("next"), self.expand_subpath("current"))
 
     def deploy(self, data):
-        self.prepare_deployment(data)
-        self.switch_to_deployment()
+        self.prepare_next_deployment(data)
+        self.switch_to_next_deployment()
 
     def rollback(self):
         os.replace(self.expand_subpath("previous"), self.expand_subpath("current"))
