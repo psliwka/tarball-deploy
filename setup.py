c99ebcfdb433931b95477566e03ea21881b3e6e4
Piotr Śliwka <contact@psliwka.info>
2019-02-02 20:27:32 +0100

    Add tests

--- setup.py
+++ setup.py
@@ -2,4 +2,8 @@
 
 from setuptools import setup
 
-setup(setup_requires=["pbr>=5.1.1,<6"], pbr=True)
+setup(
+    setup_requires=["pbr>=5.1.1,<6", "pytest-runner>=4.2,<5"],
+    tests_require=["pytest>=4.2.0,<5"],
+    pbr=True,
+)
